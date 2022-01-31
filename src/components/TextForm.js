dbbed32a1e2db2c41b3f3d948a07fa2a101bb477
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-01-31 19:02:51 +0530

    react 9 video

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,35 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleReverseClick = () => {
+        let newText = text.split("").reverse().join("");
+        // reverseString(newText);//"olleH"
+        setText(newText);
+    }
+    
+    const handleTitleClick = () => {
+        let newText = text.split(' ').map(function(word, index){
+            // First Character uppercase or lower case
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        }).join(" ");
+        setText(newText);
+    }
+
+
+    // const handleSentenceClick = () => {
+    //     let newText = text.toLowerCase().split(' ');
+    //     for(var i=0; i<newText.length;i++){
+    //         newText[i] = newText[i].charAt(0).toUpperCase()+ newText[i].substring(1);
+    //     }
+    //     return newText.join(' ');
+    //     setText(newText);
+    // }
+    
+
+    const handleClearClick = () => {
+        let newText =" ";
+        setText(newText);
+    }
 
     const handleOnChange =(event)=>{
         // console.log("On change");
@@ -26,10 +55,14 @@ export default function TextForm(props) {
         <div className="container">
             <h1>{props.heading} </h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" placeholder="Enter your text here" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleUpClick}> Uppercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleLowClick}> Lowercase</button>
+                    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear </button>
+                    <button className="btn btn-primary mx-1" onClick={handleReverseClick}>Reverse </button>
+                    <button className="btn btn-primary mx-1 my-2" onClick={handleTitleClick}>Title </button>
+                    {/* <button className="btn btn-primary mx-1 my-2" onClick={handleSentenceClick}>Sentence </button> */}
         </div>
         <div className="container my-3">
             <h2>Your text summary</h2>
